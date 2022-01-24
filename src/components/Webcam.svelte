f6d10528383a4461fbdbeef15f8db840226e1000
Christian Kaisermann <christian@kaisermann.me>
2022-01-24 22:25:57 +0100

    refactor: use normal video tag

--- src/components/Webcam.svelte
+++ src/components/Webcam.svelte
@@ -5,38 +5,11 @@
 
   let stream;
   let video;
-  let canvas;
-  let canvasContext;
 
   let isActive = false;
   let isStreamingVideo = false;
-  let isDisplayingVideo = false;
 
-  let bodyPix;
-  let bodyPixNet;
-  let animationFrame = null;
-
-  let shouldDetectPerson = false;
-
-  $: loading = isActive && (!isStreamingVideo || !isDisplayingVideo);
-
-  $: if (canvas) {
-    canvasContext = canvas.getContext('2d');
-    canvasContext.imageSmoothingEnabled = true;
-  } else {
-    canvasContext = undefined;
-  }
-
-  $: if (isStreamingVideo) {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    if (shouldDetectPerson) {
-      loadBodyPix().then(drawVideo);
-    } else {
-      drawVideo();
-    }
-  }
+  $: loading = isActive && !isStreamingVideo;
 
   onMount(() => {
     if (typeof window === 'undefined') return;
@@ -52,28 +25,7 @@
     return () => observer.disconnect();
   });
 
-  async function loadBodyPix() {
-    if (bodyPix != null) return;
-
-    await import('@tensorflow/tfjs-core');
-    await import('@tensorflow/tfjs-converter');
-    await import('@tensorflow/tfjs-backend-webgl');
-
-    bodyPix = await import('@tensorflow-models/body-pix');
-    bodyPixNet = await bodyPix.load({
-      architecture: 'MobileNetV1',
-      outputStride: 16,
-      multiplier: 0.75,
-      quantBytes: 2,
-    });
-  }
-
   async function initStream() {
-    if (video == null) {
-      video = document.createElement('video');
-      video.setAttribute('autoplay', '');
-    }
-
     stream = await window.navigator.mediaDevices
       .getUserMedia({
         video: {
@@ -104,66 +56,6 @@
     isDisplayingVideo = false;
   }
 
-  function detectPerson() {
-    return bodyPixNet
-      .segmentPerson(video, {
-        flipHorizontal: false,
-        internalResolution: 'medium',
-        segmentationThreshold: 0.6,
-      })
-      .catch((error) => console.log(error));
-  }
-
-  function drawPersonSegmentation(segmentation, auxCanvas) {
-    const auxCtx = auxCanvas.getContext('2d');
-    const mask = bodyPix.toMask(segmentation);
-
-    auxCtx.putImageData(mask, 0, 0);
-
-    // draw original image
-    canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    // use destination-out, then only masked area will be removed
-    canvasContext.save();
-    canvasContext.globalCompositeOperation = 'destination-out';
-    canvasContext.filter = 'blur(8px)';
-    canvasContext.drawImage(auxCanvas, 0, 0, canvas.width, canvas.height);
-    canvasContext.restore();
-  }
-
-  async function drawVideo() {
-    const auxCanvas = document.createElement('canvas');
-
-    async function loop() {
-      cancelAnimationFrame(animationFrame);
-      if (!isStreamingVideo) {
-        isDisplayingVideo = false;
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-        return;
-      }
-
-      auxCanvas.width = video.videoWidth;
-      auxCanvas.height = video.videoHeight;
-
-      animationFrame = requestAnimationFrame(loop);
-
-      if (shouldDetectPerson) {
-        drawPersonSegmentation(await detectPerson(), auxCanvas);
-      } else {
-        canvasContext.save();
-        canvasContext.scale(-1, 1);
-        canvasContext.drawImage(video, 0, 0, -canvas.width, canvas.height);
-        canvasContext.restore();
-      }
-
-      if (isStreamingVideo) {
-        isDisplayingVideo = true;
-      }
-    }
-
-    animationFrame = requestAnimationFrame(loop);
-  }
-
   function handleClick() {
     if (!isActive) {
       initStream();
@@ -177,18 +69,19 @@
   onDestroy(destroyStream);
 </script>
 
-<!-- eslint-disable-next-line a11y-media-has-caption -->
 <div class="video-wrapper">
   <div class="shine -video-top-left" />
   <div class="shine -video-top-right" />
   <div class="shine -video-bottom-left" />
   <div class="shine -video-bottom-right" />
-  <canvas
-    bind:this={canvas}
-    class:ready={isDisplayingVideo}
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video
+    bind:this={video}
+    class:ready={isStreamingVideo}
     class:loading
     on:click={handleClick}
     style={`--screen-ratio: ${screenRatio}`}
+    autoplay
   />
 </div>
 
@@ -199,7 +92,7 @@
       0 0 0 7px #9c9892;
   }
 
-  canvas {
+  video {
     position: relative;
     display: block;
     object-fit: cover;
@@ -216,17 +109,17 @@
     transition: filter 0.4s ease;
   }
 
-  canvas.loading {
+  video.loading {
     transition: filter 1s ease;
     filter: brightness(0.15);
   }
 
-  canvas.ready {
+  video.ready {
     background-image: url(/public/assets/images/bg-video.jpg);
   }
 
   @media (max-width: 901px) {
-    canvas {
+    video {
       max-height: 50vh;
     }
   }
